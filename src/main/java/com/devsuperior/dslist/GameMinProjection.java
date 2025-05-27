2ba9bb6bb0abf26d1a54050bda626210c9fb47f2
André_Lucas <134009910+22346-Andre@users.noreply.github.com>
2025-05-27 09:51:18 -0300

    Project created

---
+++ src/main/java/com/devsuperior/dslist/GameMinProjection.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dslist;
+
+public interface GameMinProjection {
+
+    Long getId();
+    String getTitle();
+    Integer getGameYear();
+    String getImgUrl();
+    String getShortDescription();
+    Integer getPosition();
+}
---
+++ src/main/java/com/devsuperior/dslist/controller/GameListController.java
@@ -0,0 +1,40 @@
+package com.devsuperior.dslist.controller;
+
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.dto.ReplacementDTO;
+import com.devsuperior.dslist.service.GameService;
+import com.devsuperior.dslist.service.GameListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+    @Autowired
+    private GameListService gameListService;
+
+    @Autowired
+    private GameService gameService;
+
+
+    @GetMapping
+    public List<GameListDTO> findALL(){
+        List<GameListDTO> result = gameListService.findAll();
+        return result;
+    }
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable Long listId){
+        List<GameMinDTO> result = gameService.findByList(listId);
+        return result;
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body){
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+    }
+}
